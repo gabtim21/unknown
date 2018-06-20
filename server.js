05baae453e3b4bbe385a4c48858d00d682e9797b
gabtim21 <gabtim.leon@tecsup.edu.pe>
2018-06-19 21:27:49 -0500

    Siiii

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 6060;
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/Appfmcloud');
+mongoose.Promise = global.Promise;
+
+app.listen(port);
+console.log('Ingresa al puerto' + port);
