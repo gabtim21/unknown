b145ec4434367b631bf3bfc3b1e4b5931a0d1c56
gabtim21 <gabtim.leon@tecsup.edu.pe>
2018-07-05 10:15:59 -0500

    s

--- dhamirfrontend/src/shared/axios-fmcloud.js
+++ dhamirfrontend/src/shared/axios-fmcloud.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'http://35.237.186.173/',
+	baseURL: 'http://35.237.186.173/api',
 	headers: {
 		'Authorization': localStorage.getItem('token')
 	}
